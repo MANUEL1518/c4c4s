91ba662af8e4437680cbe869e49d30b8e864a256
loquendo_118_loco@hotmail.com <loquendo_118_loco@hotmail.com>
2019-12-01 13:17:45 -0600

    v1.0

--- c4c4s.bat
+++ c4c4s.bat
@@ -1,18 +1,4 @@
 @echo off
-REM ;  _____   ___ _____   ___  _____ ______  ___ _____ 
-REM ; /  __ \ /   /  __ \ /   |/  ___|| ___ \/ _ \_   _|
-REM ; | /  \// /| | /  \// /| |\ `--. | |_/ / /_\ \| |  
-REM ; | |   / /_| | |   / /_| | `--. \| ___ \  _  || |  
-REM ; | \__/\___  | \__/\___  |/\__/ /| |_/ / | | || |  
-REM ;  \____/   |_/\____/   |_/\____(_)____/\_| |_/\_/  
-REM ;
-REM ; ##################################################
-REM ; 
-REM ; // THIS IS A TOOL TO COPY ALL FILES FROM THE USB
-REM ; // DEVICE AND YOU HAVE THE OPTION TO DELETE
-REM ; // EVERYTHING ON THE USB AFTER COPYING
-REM ; 
-REM ; ##################################################
 setlocal EnableDelayedExpansion
 :try
 IF EXIST "C:\Users\"%USERNAME%"\Desktop\NCopy" (
